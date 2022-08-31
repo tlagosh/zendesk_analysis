584c8850691b59a9cf9a9d2b3135c2dd1e2d9b8c
tlagosh <usr0060@iconstruye.com>
2022-08-31 12:10:47 -0400

    feat: dataframe cleaning and first analisys

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-import pandas
+import pandas as pd
 
+df = pd.read_csv('tickets.csv')
+
+
+print('######################\n')
+print('Filas Duplicadas: ' + str(df.duplicated().any()) + '\n')
+print('######################\n')
+
+print(df.info())
